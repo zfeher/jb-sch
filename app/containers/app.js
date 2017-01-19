cc377acb67c877c6ce04d9828594c772075b638a
Zoltan Feher <lostface@fastmail.fm>
2017-01-19 11:22:33 +0100

    Implement random table data generation

--- app/containers/app.js
+++ app/containers/app.js
@@ -1,19 +1,77 @@
 import * as React from 'react';
+import * as R from 'ramda';
 import { Table } from '../components';
+
+const MIN_TABLE_SIZE = 6;
+const MAX_TABLE_SIZE = 20;
+const SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const DEFAULT_TABLE_SIZE = MIN_TABLE_SIZE;
+
+let getRandomTableData = size => {
+  let length = size * size;
+  let halfLength = Math.floor(length / 2);
+  let symbols = createSymbols(halfLength);
+  let pairs = symbols.concat(symbols);
+
+  return R.compose(
+    R.splitEvery(size),
+    R.zipWith(
+      (id, symbol) => ({ id, symbol }),
+      R.range(0, length),
+    ),
+    randomizeArr,
+  )(pairs);
+};
+
+// TODO to common.js
+let randomizeArr = arr => (
+  R.compose(
+    R.pluck('item'),
+    R.sortBy(R.prop('num')),
+    R.zipWith(
+      (num, item) => ({ num, item }),
+      randomNumbers(arr.length),
+    )
+  )(arr)
+);
+
+// TODO to common.js
+let randomNumbers = R.compose(
+    R.map(() => Math.random()),
+    R.range(0)
+);
+
+// TODO to FP style
+let createSymbols = length => {
+  let symbols = [];
+  let num = 0;
+  let i = 0;
+
+  while (i < SYMBOLS.length) {
+    let first = SYMBOLS[i];
+    let j = 0;
+    while (num < length && j < SYMBOLS.length) {
+      let second = SYMBOLS[j];
+      symbols.push(`${first}${second}`);
+      num++;
+      j++;
+    }
+    i++;
+  }
+
+  return symbols;
+};
 
 export let App = React.createClass({
   render() {
-    let tableData = [
-      [{id: 0, symbol: 'AB'}, {id: 1, symbol: 'AC'}],
-      [{id: 2, symbol: 'AD'}, {id: 3, symbol: 'AE'}],
-    ];
+    let { tableSize, tableData } = this.state;
 
     return (
       <div
           className="app-container"
           style={{ margin: 0, padding: 0 }}>
 
-        <Table size={6} data={tableData}
+        <Table size={tableSize} data={tableData}
         />
 
       </div>
@@ -21,6 +79,11 @@ export let App = React.createClass({
   },
 
   getInitialState() {
-    return {};
+    let tableSize = DEFAULT_TABLE_SIZE;
+
+    return {
+      tableSize,
+      tableData: getRandomTableData(tableSize),
+    };
   },
 });
